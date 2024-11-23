e5e13b40d7f474a51d7db7259066cff8d8e83a13
Olyamh <olyamh234@yahoo.com>
2024-11-23 11:32:57 +0200

    add gallery done and form without styles

---
+++ src/js/form.js
@@ -0,0 +1,43 @@
+const STORAGE_KEY = "feedback-form-state";
+let formData = {
+    email: "",
+    message: ""
+};
+
+const feedbackForm = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('input[type="email"]');
+const inputMessage = document.querySelector('textarea[name="message"]');
+
+feedbackForm.addEventListener('input', onFeedbackForm);
+popupText();
+feedbackForm.addEventListener('submit', checkSubmit);
+
+function onFeedbackForm(){
+    const emailValue = inputEmail.value;
+    const textareaValue = inputMessage.value;
+    formData.email = emailValue;
+    formData.message = textareaValue;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function popupText(){
+    const infoGet = localStorage.getItem(STORAGE_KEY);
+    const info = JSON.parse(infoGet);
+    
+    if (info){
+        inputEmail.value = info.email;
+        inputMessage.value = info.message;
+    }
+}
+
+function checkSubmit (event){
+    event.preventDefault();
+  
+    if (!inputEmail.value || !inputMessage.value) {
+        alert('All form fields must be filled in');
+      } else {
+        console.log({ email: inputEmail.value, message: inputMessage.value });;
+    }
+  }
+
